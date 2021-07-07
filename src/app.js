cfc9173efbda4c314161760036666562b7453f7a
RubenGonzalez1983 <gonzalezjuarez83@gmail.com>
2021-07-07 01:02:22 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,14 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const unorderedList = document.getElementById("list-of-domains");
+  unorderedList.innerHTML = ;
+
 };
