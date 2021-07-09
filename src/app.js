acb70fdff4de73be0242e86041a5c0709d9e5b7b
RubenGonzalez1983 <gonzalezjuarez83@gmail.com>
2021-07-09 22:26:29 +0000

    added domains as li

--- src/app.js
+++ src/app.js
@@ -4,13 +4,14 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-
 window.onload = function() {
   function generateDomain() {
     let pronoun = ["my", "your", "our"];
     let adj = ["invincible", "flying", "holographic"];
     let noun = ["wizard", "possum", "joggler"];
     let extension = [".com", ".io", ".net"];
+
+    let ul = document.getElementById("list-of-domains");
 
     let myArray = [];
 
@@ -19,12 +20,16 @@ window.onload = function() {
         for (let n = 0; n < noun.length; n++) {
           for (let e = 0; e < extension.length; e++) {
             myArray.push(pronoun[p] + adj[a] + noun[n] + extension[e]);
+
+            let li = document.createElement("LI");
+            li.innerHTML = pronoun[p] + adj[a] + noun[n] + extension[e];
+            ul.appendChild(li);
           }
         }
       }
     }
     return myArray;
   }
-
-  document.getElementById("list-of-domains").innerHTML = generateDomain();
+  generateDomain();
+  //document.getElementById("list-of-domains").innerHTML = generateDomain();
 };
